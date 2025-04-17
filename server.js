bcf57db5176acfa4a14c0affb00772635666d39b
Yazhinithiruma <104961426+Yazhinithiruma@users.noreply.github.com>
2025-04-17 22:54:55 +0530

    Update server.js

--- server.js
+++ server.js
@@ -61,6 +61,6 @@ app.delete('/api/history/:index', (req, res) => {
 
 // Start the server
 const PORT = 3000;
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
